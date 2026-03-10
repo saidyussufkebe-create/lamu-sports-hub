72916d83aa5281f7d992799f2db5a0aa5721b14c
saidyussufkebe-create <saidyussufkebe@gmail.com>
2026-03-10 07:11:29 +0300

    Create News.tsx

---
+++ src/pages/News.tsx
@@ -0,0 +1,22 @@
+function News(){
+
+return(
+
+<div>
+
+<h2>Latest Videos</h2>
+
+<iframe
+width="100%"
+height="250"
+src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+allowFullScreen
+></iframe>
+
+</div>
+
+)
+
+}
+
+export default News
