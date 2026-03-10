8a43084b1c5125239ae1eeebcb8de44904dd3116
saidyussufkebe-create <saidyussufkebe@gmail.com>
2026-03-10 07:12:22 +0300

    Create Admin.tsx

---
+++ src/pages/Admin.tsx
@@ -0,0 +1,19 @@
+function Admin(){
+
+return(
+
+<div>
+
+<h1>Admin Panel</h1>
+
+<button>Add Team</button>
+<button>Add Player</button>
+<button>Add Match</button>
+
+</div>
+
+)
+
+}
+
+export default Admin
